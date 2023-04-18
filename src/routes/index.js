5f235d85d242a91c8b242725c6d8e7b94385292b
Igor Follador <igorledf@hotmail.com>
2023-04-18 12:14:52 -0300

    Add lib CORS to comunication with web application

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,17 +1,11 @@
+const cors = require('cors');
 const publicRoutes = require('./publicRoutes');
 const userRoutes = require('./userRoutes');
 const conversationRoutes = require('./conversationRoutes');
 const jwt = require('jsonwebtoken');
 
 module.exports = (app, express) => {
-    app.use(function(req, res, next) {
-        res.setHeader('Access-Control-Allow-Origin', '*');
-        res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
-        res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-        res.setHeader('Access-Control-Allow-Credentials', true);
-        next();
-    });
-
+    app.use(cors());
     app.use(express.json())
     app.use(express.urlencoded({ extended: true}))
 
