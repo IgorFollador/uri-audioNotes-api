2ccce16e820d77a6735256f1c596efbed73808fe
Igor Follador <igorledf@hotmail.com>
2023-04-17 21:58:39 -0300

    Create file to training NLP

--- src/services/ConversationService.js
+++ src/services/ConversationService.js
@@ -1,5 +1,6 @@
-const natural = require('natural'); 
+const trainingData = require('../../trainingData.json');
 
+const natural = require("natural"); 
 class ConversationService {
     
     static createCategory(data) {
@@ -7,19 +8,15 @@ class ConversationService {
         const categories = new natural.BayesClassifier();
 
         // Adding the categories
-        categories.addDocument('estudo', ['estudar', 'escola', 'faculdade']);
-        categories.addDocument('casa', ['morar', 'residência', 'lar', 'casa']);
-        categories.addDocument('trabalho', ['emprego', 'empresa', 'profissão']);
+        categories.addDocument("ideia", ["ideias", "pensamento", "profissão"]);
+        categories.addDocument("estudo", ["estudar", "escola", "faculdade"]);
+        categories.addDocument("casa", ["morar", "residência", "lar", "casa"]);
+        categories.addDocument("trabalho", ["emprego", "empresa", "profissão"]);
 
         // Defining the classifier based on categories
         const classifier = new natural.BayesClassifier();
 
-        // Training the classifier with examples from each category
-        const trainingData = [
-        { text: 'Estou estudando para uma prova importante', category: 'estudo' },
-        { text: 'Comprei um apartamento novo', category: 'casa' },
-        { text: 'Gosto do meu trabalho, mas as vezes é cansativo', category: 'trabalho' },
-        ];
+        // Training the classifier with examples from each "category"
         trainingData.forEach((example) => classifier.addDocument(example.text, example.category));
         classifier.train();
 
